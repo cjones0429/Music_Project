4170a525cf4e2f893a67cdce45a898e9d65e2009
cjones0429 <cejones1997@aim.com>
2021-06-11 17:20:57 -0400

    attempting bonus question - likes. added class LikeSong. added get object, get, and put methods to class

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
-    album = models.CharField(max_length=50)
-    release_date = models.DateTimeField()
+    album = models.CharField(max_length=50, blank=True, null=True)
+    release_date = models.DateTimeField(blank=True, null=True)
+    likes = models.PositiveIntegerField(blank=True, null=True)
