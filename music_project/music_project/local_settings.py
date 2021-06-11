68165c52e4219375d4ef89e238a8ae663d14901d
cjones0429 <cejones1997@aim.com>
2021-06-11 14:51:54 -0400

    finished tutorial

---
+++ music_project/music_project/local_settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = '$geisrfe=4t)$baxe38ai(^qzr+r(7q1@xunr*ln4q#3b$0nlu'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'music_library_database',
+        'USER': 'root',
+        'PASSWORD': 'staJ6N14',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
